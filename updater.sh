074888785181e8114c2d7db186eb8861e92916d9
Duality224 <98560612+Duality224@users.noreply.github.com>
2022-01-28 12:03:55 +1300

    Create updater.sh
    Core updater file

---
+++ updater.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#checking sudo rights and asking for password if needed
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
+#closing vivaldi
+killall vivaldi-bin
+
+#updating apt packages
+apt-get update
+apt-get upgrade
+
+#updating flatpak packages
+flatpak update
+
+#restarting vivaldi as a standalone process and exiting terminal window
+sudo -u [USERNAME] /path/to/standalone.sh /opt/vivaldi/vivaldi
+exit
