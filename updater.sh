77da05d91cc705775809967d3484493577bd7e29
Duality224 <98560612+Duality224@users.noreply.github.com>
2022-03-23 20:20:01 +1300

    Add Steven Black's hosts file update line

--- updater.sh
+++ updater.sh
@@ -16,6 +16,10 @@ apt-get upgrade
 #updating flatpak packages
 flatpak update
 
+#updating Steven Black's hosts file
+cd /home/mattk/Documents/Scripts/_unmanaged-apps/_self-updating/StevenBlack-hosts
+python3 ./updateHostsFile.py --auto --replace --output /etc --flush-dns-cache --skipstatichosts --nogendata --blacklist ./blacklist --whitelist ./whitelist
+
 #restarting vivaldi as a standalone process and exiting terminal window
 sudo -u USERNAME /path/to/standalone.sh /opt/vivaldi/vivaldi
 exit
